8a9c99acea1194b8aefb6aba221efaab5e0a4faa
Paulo Ortolan (ext.) <Paulo.Ortolan@ctw.bmwgroup.com>
2020-08-20 18:05:32 +0100

    Modifying translator to include sequence invoking on inserts

--- documents/translators/dartlang/Location.dart
+++ documents/translators/dartlang/Location.dart
@@ -8,7 +8,7 @@ class Location {
   int country_id;
 
   String toSQLInsert() {
-    return "INSERT INTO location (day, month, adm_level_1, adm_level_2, latitude, longitude, country_id) VALUES (${day}, ${month}, '${normatize(adminLevel1)}', '${adminLevel2}', ${latitude}, ${longitude}, ${country_id});\n";
+    return "INSERT INTO location (id, day, month, adm_level_1, adm_level_2, latitude, longitude, country_id) VALUES (nextval('seq_location'), ${day}, ${month}, '${normatize(adminLevel1)}', '${adminLevel2}', ${latitude}, ${longitude}, ${country_id});\n";
   }
 
   String normatize(String value) {
